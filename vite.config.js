71e2707f94763079c1268f17936738c0411ae06e
Bhanuanjaneyakumar-meghaai <anjaneya.kumar@meghaai.com>
2026-04-29 18:23:50 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bel-dashboard/' // MUST match repo name
+  base: '/bel/' // MUST match repo name
 })
